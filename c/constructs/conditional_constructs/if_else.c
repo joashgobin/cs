672df979ed3ab755c1b8bb0c8eccae8524c88f2e
Joash Gobin <joashgobin@gmail.com>
2023-12-20 11:12:57 -0400

    If-else c file modified

--- c/constructs/conditional_constructs/if_else.c
+++ c/constructs/conditional_constructs/if_else.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+int main()
 {
-    printf("%d\n",13);
+    int age;
+    printf("Please enter your age: ");
+    scanf("%d",&age);
+    if(age>=18){
+        printf("You are an adult\n");
+    }else{
+        printf("You are not an adult as yet\n");
+    }
     return 0;
 }
