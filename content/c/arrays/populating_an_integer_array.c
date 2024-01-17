f2e9e49376c0cfe606bbd0ffd4a081bd21fcfc3d
joashgobin <joashgobin@gmail.com>
2024-01-17 09:46:20 -0400

    Tweaks circa Wed Jan 17 09:24:09 -04 2024

--- content/c/arrays/populating_an_integer_array.c
+++ content/c/arrays/populating_an_integer_array.c
@@ -4,8 +4,11 @@
 int main(){
     double numbers[90];
     for (int i=0;i<90;i++){
+
+        // casting i+1 as a double and converting the angle to radians
         numbers[i] = sin(M_PI/180.0*(double)(i+1));
         printf("The angle at index %d is sin(%d)=%g\n",i,i+1,numbers[i]);
+
     }
     return 0;
 }
--- content/c/arrays/traversing_a_string.c
+++ content/c/arrays/traversing_a_string.c
@@ -2,17 +2,24 @@
 #include <string.h>
 
 int main(){
+    // there is no need to manually count the string size so we use []
     char word[] = "Hello there comrade";
     int count = 0;
 
+    // traversing the string in the forward direction
     for (int i=0;i<strlen(word);i++){
+        
+        // printing the current letter
         printf("The letter at index %d is %c\n",i,word[i]);
+        
+        // incrementing count if the current letter is an 'a'
         if (word[i]=='a'){
             count++;
         }
     }
     printf("We found %d a's in \"%s\"\n",count,word);
 
+    // traversing the string in reverse
     printf("The reversed word is ");
     for (int i=strlen(word)-1;i>=0;i--){
         printf("%c",word[i]);
